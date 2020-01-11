f8bba16790d46aafcabe19ea278e2da772681006
Jorge Hevia <jmhev@outlook.com>
2020-01-11 16:53:56 +0100

    Add toy exploring Higher Order Functions in C++

--- recursive/c++/hof_in_cpp.cc
+++ recursive/c++/hof_in_cpp.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 template<typename T>
 int add(int a, int b, T fn)
@@ -11,8 +12,21 @@ int pow(int a)
     return a * a;
 }
 
+template<typename Iterator, typename T>
+int sumList(Iterator begin, Iterator end, T fn)
+{
+    if (begin + 1 == end) { return *begin; }
+    return add(*begin, sumList(begin + 1, end, fn), fn);
+}
+
 int main(int argc, char const *argv[])
 {
+    std::cout << "add(1, -2, pow)" << std::endl;
     std::cout << add(1, -2, pow) << std::endl;
+
+    std::vector<int> numList {1, -2};
+    std::cout << "numList: {1, -2}\n";
+    std::cout << "sumList(numList.begin(), numList.end(), pow)" << std::endl;
+    std::cout << sumList(numList.begin(), numList.end(), pow) << std::endl;
     return 0;
 }
