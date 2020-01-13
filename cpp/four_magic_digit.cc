1cd5ee86e132cf8c43eb8899831b774575b23aa4
Jorge Hevia <jmhev@outlook.com>
2020-01-13 15:14:29 +0100

    Reident file following Mozilla guidelines

--- cpp/four_magic_digit.cc
+++ cpp/four_magic_digit.cc
@@ -3,7 +3,10 @@
 #include <iostream>
 #include <vector>
 
-template <typename T> void print_vector(const std::vector<T> &v) {
+template<typename T>
+void
+print_vector(const std::vector<T>& v)
+{
   std::cout << "{ ";
   for (auto i : v) {
     std::cout << i << " ";
@@ -11,7 +14,9 @@ template <typename T> void print_vector(const std::vector<T> &v) {
   std::cout << "}" << std::endl;
 }
 
-int vector_to_int(const std::vector<int> &vector) {
+int
+vector_to_int(const std::vector<int>& vector)
+{
   int result = 0;
 
   for (int i = 0; i < vector.size(); ++i) {
@@ -21,7 +26,9 @@ int vector_to_int(const std::vector<int> &vector) {
   return result;
 }
 
-std::vector<int> int_to_vector(int number) {
+std::vector<int>
+int_to_vector(int number)
+{
   std::vector<int> result{};
 
   while (number > 0) {
@@ -36,10 +43,12 @@ std::vector<int> int_to_vector(int number) {
   return result;
 }
 
-std::vector<int> four_magic_number(const std::vector<int> &digits) {
+std::vector<int>
+four_magic_number(const std::vector<int>& digits)
+{
   if (vector_to_int(digits) == 6174) {
     std::cout << "yay!" << std::endl;
-    return {6, 1, 7, 4};
+    return { 6, 1, 7, 4 };
   }
 
   std::vector<int> ascend = digits;
@@ -51,8 +60,8 @@ std::vector<int> four_magic_number(const std::vector<int> &digits) {
   int descend_number = vector_to_int(descend);
 
   std::vector<int> magic_digits = int_to_vector(
-      (ascend_number < descend_number) ? descend_number - ascend_number
-                                       : ascend_number - descend_number);
+    (ascend_number < descend_number) ? descend_number - ascend_number
+                                     : ascend_number - descend_number);
 
   std::cout << "\nGiven: ";
   print_vector(digits);
@@ -62,9 +71,10 @@ std::vector<int> four_magic_number(const std::vector<int> &digits) {
   return four_magic_number(magic_digits);
 }
 
-int main(int argc, char const *argv[]) {
-
-  std::vector<int> digits{1, 5, 4, 7};
+int
+main(int argc, char const* argv[])
+{
+  std::vector<int> digits{ 1, 5, 4, 7 };
   int number = 1547;
 
   four_magic_number(digits);
