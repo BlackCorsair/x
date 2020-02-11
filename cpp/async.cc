5610f661bcfecb6eab099ea3785c1a02e5145775
Jorge Hevia <jmhev@outlook.com>
2020-02-11 09:45:37 +0100

    Add asynx example

---
+++ cpp/async.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <future>
+#include <cmath>
+#include <vector>
+#include <thread>
+
+long long tedious_calculation(int x)
+{
+    return x*x;
+}
+
+long long recursion_magic(int n)
+{
+    if (n == 1)
+        return 1;
+    return n * recursion_magic(n - 1);
+}
+int main()
+{
+    std::vector<std::thread> thread_list;
+    std::vector<std::future<long long>> futures;
+
+    for (int i = 0; i < 100; ++i)
+    {
+        if (i % 2 == 0)
+        {
+            std::packaged_task<long long(int)> task(tedious_calculation);
+            futures.push_back(task.get_future());
+            thread_list.push_back(
+                std::thread(std::move(task), i)
+            );
+        } else {
+            std::packaged_task<long long(int)> task(recursion_magic);
+            futures.push_back(task.get_future());
+            thread_list.push_back(
+                std::thread(std::move(task), i)
+            );
+        }
+    }
+
+    for (auto& thread: thread_list)
+    {
+        thread.join();
+    }
+
+    for (auto& future: futures)
+    {
+        std::cout << "future:\t" << future.get() << "\n";
+    }
+}
